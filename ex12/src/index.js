41774b93481f959b8cc13a77674df1de0dd173fb
Almir2021 <bosnjicalmir@gmail.com>
2021-09-17 21:34:03 +0200

    Vjezba 12 zadnje slovo

---
+++ ex12/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+
+    //only change code below this line
+    var lastName = "Tesla";
+    var lastLetterofLastName = lastName[lastName.length - 1];
+    //only change code above this line 
+    return lastLetterofLastName;
+
+
+}
+
+console.log(main());
+
+module.exports = main;
