9d00538d5b73b7786e7a513d70689f3b0cbfb164
Almir2021 <bosnjicalmir@gmail.com>
2021-09-17 20:00:23 +0200

    Vjezba broj 9  firstLetterofLastName

---
+++ ex09/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+
+    //only change code below this line
+    var lastName = "Doe";
+    var firstLetterofLastName = lastName[0];
+    //only change code above this line 
+    return firstLetterofLastName;
+
+
+}
+
+console.log(main());
+
+module.exports = main;
