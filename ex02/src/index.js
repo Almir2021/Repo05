4462ce15b83f9d5daeb07dc62f013c150e9bd7b9
Almir2021 <bosnjicalmir@gmail.com>
2021-09-17 16:01:36 +0200

    Vjezba 2 sa jednoznacnim navodnim znacima

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+
+    //only change code below this line
+    var myLink = '<a href="http://www.example.com" target="blank">Link</a> '
+    //only change code above this line 
+    return myLink;
+
+
+}
+
+console.log(main());
+
+module.exports = main;
